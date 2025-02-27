a8b68f1968cd96619b6d20c24f8da59a6211001e
BAHIA <larissa.cordeiro@alunorosario.com.br>
2025-02-27 15:46:47 -0300

    init commit

---
+++ Assets/revisao.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revisao : MonoBehaviour
+{
+    int = nInteriro = 1;
+    float = nQuebrado = 1.2f;
+    string = nTexto = "Lari";
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("eu sou legal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
